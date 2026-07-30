e69bdc4dfa5cebc72ab0ec0f0985bf72d2274f21
asterling8516 <142363406+asterling8516@users.noreply.github.com>
2026-07-29 23:47:54 -0500

    feat: add custom visual styling, hide Assets folder in explorer, and set search to desktop-only

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,12 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./.quartz/plugins"
+
+// Must be placed before loadQuartzConfig()
+ExternalPlugin.Explorer({
+  filterFn: (node) => {
+    return node.name !== "Assets"
+  },
+})
 
 const config = await loadQuartzConfig()
 export default config
