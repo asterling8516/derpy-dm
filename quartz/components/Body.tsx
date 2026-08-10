1a143bf7f3ea09d55bb6446341f9290111b0e3f8
asterling8516 <142363406+asterling8516@users.noreply.github.com>
2026-08-10 16:46:57 -0500

    feat: add fantasy-themed styling and image zoom functionality to site components

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,7 +1,11 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
+import zoomScript from "./scripts/zoom.inline"
+
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
   return <div id="quartz-body">{children}</div>
 }
+
+Body.afterDOMLoaded = zoomScript
 
 export default (() => Body) satisfies QuartzComponentConstructor
