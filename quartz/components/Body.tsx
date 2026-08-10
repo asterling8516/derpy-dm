3bf70cef4a8b940db2bfe7ab798b358ba8fd7d0c
asterling8516 <142363406+asterling8516@users.noreply.github.com>
2026-08-10 17:15:17 -0500

    feat: removed custom Body component and add fantasy-themed CSS styles

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,11 +1,7 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
-import zoomScript from "./scripts/zoom.inline"
-
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
   return <div id="quartz-body">{children}</div>
 }
-
-Body.afterDOMLoaded = zoomScript
 
 export default (() => Body) satisfies QuartzComponentConstructor
